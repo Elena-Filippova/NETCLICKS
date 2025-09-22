d0568d5555a1c5b49ffe8740640cf197b6c71c4c
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2025-09-22 16:26:07 +0300

    Add files via upload

---
+++ src/app/constants/movies.constants.ts
@@ -0,0 +1,58 @@
+import { Movie } from "../models/movie.model";
+
+export const MOVIES: Movie[] = [
+  {
+    id: 0,
+    title: "Марсианин,",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./img/movie.png",
+    link: "./movie.html",
+  },
+  {
+    id: 1,
+    title: "Марсианин,",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./img/movie.png",
+    link: "./movie.html",
+  },
+  {
+    id: 2,
+    title: "Марсианин,",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./img/movie.png",
+    link: "./movie.html",
+  },
+  {
+    id: 3,
+    title: "Марсианин,",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./img/movie.png",
+    link: "./movie.html",
+  },
+  {
+    id: 4,
+    title: "Марсианин,",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./img/movie.png",
+    link: "./movie.html",
+  },
+  {
+    id: 5,
+    title: "Марсианин,",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    image: "./img/movie.png",
+    link: "./movie.html",
+  },
+]
---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+  id: number;
+  title: string;
+  originalTitle: string;
+  category: string;
+  rating: number;
+  image: string;
+  link: string;
+}
